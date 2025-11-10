b3c6c4841e6b6dd4e5723dfbe4e29d43b8a1473f
Geraltwolf-bit <geraltwolf@yandex.ru>
2025-11-10 23:16:22 +0300

    fixed a bug

--- crypto_dashboard/app.py
+++ crypto_dashboard/app.py
@@ -133,193 +133,180 @@ def main():
         with rcol2:
             st.header(f"{recommendation}")
             st.subheader("Because:")
-            st.markdown("""
-            <div style="text-align: center; font-size: 40px; margin: 20px 0; color: #fcf7f7;">
-                <span style="margin: 0 20px;">&#8601;</span>
-                <span style="margin: 0 20px;">&#8595;</span>
-                <span style="margin: 0 20px;">&#8600;</span>
-            </div>
-            """, unsafe_allow_html=True)
+            st.header(":point_down:")
 
-        #Initialize main columns:
-        col1, col2, col3 = st.columns(3)
 
-        #col1 - F&G Index:
-        with col1:
-            st.subheader(f"📈 F&G long-term trend: {lt_trend}")
-
-            st.subheader('Montly trend')
+        #F&G Index:
+        st.subheader(f"📈 F&G long-term trend: {lt_trend}")
                 
-            # Line chart
-            df_fg_index = get_index(URL, timeout = 10, limit=30, format = 'json')
-            fig_line = px.line(
-                df_fg_index,
-                x='date', 
-                y='value',
-                labels={'date': 'Date', 'value': 'Index Value'},
-                color_discrete_sequence=["#1fb42b"]
-            )
-            
-            # Add horizontal lines for different zones
-            ap = 'top left'
-            af = dict(size=16, family='Arial')
-            fig_line.add_hline(y=25,
-                            line_dash="dash",
-                            line_color="red",
-                            annotation_text="Extreme Fear",
-                            annotation_position=ap,
-                            annotation_font=af)
-            fig_line.add_hline(y=45,
-                            line_dash="dash",
-                            line_color="orange",
-                            annotation_text="Fear",
-                            annotation_position=ap,
-                            annotation_font=af)
-            fig_line.add_hline(y=55,
-                            line_dash="dash", 
-                            line_color="yellow",
-                            annotation_text="Neutral",
-                            annotation_position=ap,
-                            annotation_font=af)
-            fig_line.add_hline(y=75,
-                            line_dash="dash",
-                            line_color="lightgreen",
-                            annotation_text="Greed",
-                            annotation_position=ap,
-                            annotation_font=af)
-            fig_line.add_hline(y=100,
-                            line_dash="dash",
-                            line_color="green",
-                            annotation_text="Extreme Greed",
-                            annotation_position=ap,
-                            annotation_font=af)
-            fig_line.update_layout(
-                height=500,
-                xaxis_title="Date",
-                yaxis_title="Index Value (0-100)",
-                yaxis=dict(range=[0, 100])
-            )
-            st.plotly_chart(fig_line, use_container_width=True)
+        # Line chart
+        df_fg_index = get_index(URL, timeout = 10, limit=30, format = 'json')
+        fig_line = px.line(
+            df_fg_index,
+            x='date', 
+            y='value',
+            labels={'date': 'Date', 'value': 'F&G Index'},
+            color_discrete_sequence=["#1fb42b"]
+        )
+        
+        # Add horizontal lines for different zones
+        ap = 'top left'
+        af = dict(size=16, family='Arial')
+        fig_line.add_hline(y=25,
+                        line_dash="dash",
+                        line_color="red",
+                        annotation_text="Extreme Fear",
+                        annotation_position=ap,
+                        annotation_font=af)
+        fig_line.add_hline(y=45,
+                        line_dash="dash",
+                        line_color="orange",
+                        annotation_text="Fear",
+                        annotation_position=ap,
+                        annotation_font=af)
+        fig_line.add_hline(y=55,
+                        line_dash="dash", 
+                        line_color="yellow",
+                        annotation_text="Neutral",
+                        annotation_position=ap,
+                        annotation_font=af)
+        fig_line.add_hline(y=75,
+                        line_dash="dash",
+                        line_color="lightgreen",
+                        annotation_text="Greed",
+                        annotation_position=ap,
+                        annotation_font=af)
+        fig_line.add_hline(y=100,
+                        line_dash="dash",
+                        line_color="green",
+                        annotation_text="Extreme Greed",
+                        annotation_position=ap,
+                        annotation_font=af)
+        fig_line.update_layout(
+            height=500,
+            xaxis_title="Date",
+            yaxis_title="Index Value (0-100)",
+            yaxis=dict(range=[0, 100])
+        )
+        st.plotly_chart(fig_line, use_container_width=True)
         
         #col2 - Stockmarket
-        with col2:
-            raw_sm = get_raw_stockmarket_data()
-            sm = get_yearly_stockmarket_data_for_dashboard(raw_sm)
-            curr_sm = sm.iloc[-1]['stockmarket_value']
-            monthly_sm = get_montly_stockmarket_trend(raw_sm)
-            yearly_sm = get_yearly_stockmarket_trend(raw_sm)
-            month_sm = sm.iloc[-25]['stockmarket_value']
-            year_sm = sm.iloc[0]['stockmarket_value']
+        raw_sm = get_raw_stockmarket_data()
+        sm = get_yearly_stockmarket_data_for_dashboard(raw_sm)
+        curr_sm = sm.iloc[-1]['stockmarket_value']
+        monthly_sm = get_montly_stockmarket_trend(raw_sm)
+        yearly_sm = get_yearly_stockmarket_trend(raw_sm)
+        month_sm = sm.iloc[-25]['stockmarket_value']
+        year_sm = sm.iloc[0]['stockmarket_value']
 
-            #calculate montly rise / fall:
-            monthly_change = (
-                (sm.iloc[-1]['stockmarket_value'] - sm.iloc[-25]['stockmarket_value'])
-                / sm.iloc[-25]['stockmarket_value']
-                ) * 100
-            
-            #calculate yearly rise / fall:
-            yearly_change = (
-                (sm.iloc[-1]['stockmarket_value'] - sm.iloc[0]['stockmarket_value'])
-                / sm.iloc[0]['stockmarket_value']
-                ) * 100
-
-            monthly_trend_value = monthly_sm.iloc[0]['stockmarket']
-            st.subheader(f"👩‍💼 Stock market: {monthly_trend_value}")
-
-            st.markdown("""
-            <style>
-            div[data-testid='metric-container']{
-                        text-align: center !important;
-                        justify-content: center !important;
-            }
-            [data-testid='stMetricValue']{
-                        font-size: 20px;
-                        text-align: center !important;
-                        justify-content: center !important;
-                        align-items: center !important;
-            }
-            [data-testid='stMetricLabel']{
-                        font-size: 16px;
-                        text-align: center !important;
-                        justify-content: center !important;
-            }
-            </style>
-            """, unsafe_allow_html=True)
-            cur_col = st.columns(1)[0]
-            with cur_col:
-                st.metric('Current S&P 500 value', f'{curr_sm:.2f}', border=True)
-
-            scol1, scol2 = st.columns(2)
-            
-            with scol1:
-                st.metric('S&P 500 a month ago', f'{month_sm:.2f}', border=True, height='stretch', width='stretch')
-            with scol2:
-                st.metric('S&P 500 a year ago', f'{year_sm:.2f}', border=True, height='stretch', width='stretch')
-
-            sm1_col1, sm1_col2 = st.columns(2)
-
-            with sm1_col1:
-                monthly_trend_value = monthly_sm.iloc[0]['stockmarket']
-                monthly_direction = 'normal' if monthly_trend_value == 'Rising' else 'inverse' if monthly_trend_value == 'Falling' else 'off'
-            
-                st.metric(
-                    label = 'Monthly trend',
-                    value=monthly_trend_value,
-                    delta = f"{monthly_change:.2f}%",
-                    delta_color=monthly_direction, border=True, height='stretch', width='stretch')
-            
-            with sm1_col2:
-                yearly_trend_value = yearly_sm.iloc[0]['stockmarket']
-                yearly_direction = 'normal' if monthly_trend_value == 'Rising' else 'inverse' if monthly_trend_value == 'Falling' else 'off'
-            
-                st.metric(
-                    label = 'Yearly trend',
-                    value=yearly_trend_value,
-                    delta = f"{yearly_change:.2f}%",
-                    delta_color=yearly_direction, border=True, height='stretch', width='stretch')
-                
-            #Stockmarket long-term trend
-            st.subheader('Yearly trend')
-            st.area_chart(sm.set_index('date')['stockmarket_value'])
+        #calculate montly rise / fall:
+        monthly_change = (
+            (sm.iloc[-1]['stockmarket_value'] - sm.iloc[-25]['stockmarket_value'])
+            / sm.iloc[-25]['stockmarket_value']
+            ) * 100
         
-        #col3 - Inflation
-        with col3:
-            #Inflation
-            cpi = get_cpi()
-            inflation = get_inflation(cpi)
-            current_inflation = inflation.iloc[0]['current_inflation']
-            inflation_growth = inflation.iloc[0]['inflation_growth']
-            inflation_estimate = inflation.iloc[0]['inflation_estimate']
-            
-            st.subheader(f"💸 Inflation: {inflation_estimate}")
-            inf_col1, inf_col2 = st.columns(2)
-            with inf_col1:
-                st.metric(label='Current', value = f"{current_inflation}%", border=True, height='stretch', width='stretch')
-            with inf_col2:
-                st.metric(label='Growth', value = f"{inflation_growth}%", border=True, height='stretch', width='stretch')
+        #calculate yearly rise / fall:
+        yearly_change = (
+            (sm.iloc[-1]['stockmarket_value'] - sm.iloc[0]['stockmarket_value'])
+            / sm.iloc[0]['stockmarket_value']
+            ) * 100
 
-            #Long-term inflatioin trend
-            cpi = get_cpi()
-            plot_cpi = cpi.copy()
-            plot_cpi = plot_cpi[:-2]
-            plot_cpi.sort_values(by = 'date', ascending=True, inplace=True)
-            st.subheader("Yearly trend")
+        monthly_trend_value = monthly_sm.iloc[0]['stockmarket']
+        st.subheader(f"👩‍💼 Stock market: {monthly_trend_value}")
+
+        st.markdown("""
+        <style>
+        div[data-testid='metric-container']{
+                    text-align: center !important;
+                    justify-content: center !important;
+        }
+        [data-testid='stMetricValue']{
+                    font-size: 20px;
+                    text-align: center !important;
+                    justify-content: center !important;
+                    align-items: center !important;
+        }
+        [data-testid='stMetricLabel']{
+                    font-size: 16px;
+                    text-align: center !important;
+                    justify-content: center !important;
+        }
+        </style>
+        """, unsafe_allow_html=True)
+        cur_col = st.columns(1)[0]
+        with cur_col:
+            st.metric('Current S&P 500 value', f'{curr_sm:.2f}', border=True)
+
+        scol1, scol2 = st.columns(2)
+        
+        with scol1:
+            st.metric('S&P 500 a month ago', f'{month_sm:.2f}', border=True, height='stretch', width='stretch')
+        with scol2:
+            st.metric('S&P 500 a year ago', f'{year_sm:.2f}', border=True, height='stretch', width='stretch')
+
+        sm1_col1, sm1_col2 = st.columns(2)
+
+        with sm1_col1:
+            monthly_trend_value = monthly_sm.iloc[0]['stockmarket']
+            monthly_direction = 'normal' if monthly_trend_value == 'Rising' else 'inverse' if monthly_trend_value == 'Falling' else 'off'
+        
+            st.metric(
+                label = 'Monthly trend',
+                value=monthly_trend_value,
+                delta = f"{monthly_change:.2f}%",
+                delta_color=monthly_direction, border=True, height='stretch', width='stretch')
+        
+        with sm1_col2:
+            yearly_trend_value = yearly_sm.iloc[0]['stockmarket']
+            yearly_direction = 'normal' if monthly_trend_value == 'Rising' else 'inverse' if monthly_trend_value == 'Falling' else 'off'
+        
+            st.metric(
+                label = 'Yearly trend',
+                value=yearly_trend_value,
+                delta = f"{yearly_change:.2f}%",
+                delta_color=yearly_direction, border=True, height='stretch', width='stretch')
             
-            fig = px.bar(
-            plot_cpi, 
-            x='date_formatted', 
-            y='hist_inf_rate',
-            labels={'hist_inf_rate': 'Monthly Inflation Rate (%)', 'date_formatted': 'Date'},
-            color='hist_inf_rate',
-            color_continuous_scale='RdYlGn_r')
-            fig.update_traces(
-            texttemplate='%{y:.1f}%',
-            textposition='outside')
-            fig.update_layout(
-            xaxis_tickangle=-45,
-            showlegend=False,
-            height=400)
-            st.plotly_chart(fig, use_container_width=True)
+        #Stockmarket long-term trend
+        st.area_chart(sm.set_index('date')['stockmarket_value'])
+        
+    
+        #Inflation
+        cpi = get_cpi()
+        inflation = get_inflation(cpi)
+        current_inflation = inflation.iloc[0]['current_inflation']
+        inflation_growth = inflation.iloc[0]['inflation_growth']
+        inflation_estimate = inflation.iloc[0]['inflation_estimate']
+        
+        st.subheader(f"💸 Inflation: {inflation_estimate}")
+        inf_col1, inf_col2, inf_col3 = st.columns(3)
+        with inf_col1:
+            st.metric(label='Central Bank target', value=f"{2.0}%", border=True, height='stretch', width='stretch')
+        with inf_col2:
+            st.metric(label='Current', value = f"{current_inflation}%", border=True, height='stretch', width='stretch')
+        with inf_col3:
+            st.metric(label='Growth', value = f"{inflation_growth}%", border=True, height='stretch', width='stretch')
+
+        #Long-term inflatioin trend
+        cpi = get_cpi()
+        plot_cpi = cpi.copy()
+        plot_cpi = plot_cpi[:-2]
+        plot_cpi.sort_values(by = 'date', ascending=True, inplace=True)
+        
+        fig = px.bar(
+        plot_cpi, 
+        x='date_formatted', 
+        y='hist_inf_rate',
+        labels={'hist_inf_rate': 'Monthly Inflation Rate (%)', 'date_formatted': 'Date'},
+        color='hist_inf_rate',
+        color_continuous_scale='RdYlGn_r')
+        fig.update_traces(
+        texttemplate='%{y:.1f}%',
+        textposition='outside')
+        fig.update_layout(
+        xaxis_tickangle=-45,
+        showlegend=False,
+        height=500, width=None, autosize=True)
+        st.plotly_chart(fig, use_container_width=True)
 
 if __name__ == "__main__":
     main()
--- crypto_dashboard/inflation.py
+++ crypto_dashboard/inflation.py
@@ -1,9 +1,5 @@
-import os
-from dotenv import load_dotenv
-load_dotenv()
-
-infl_api_url = os.getenv('infl_api_url')
-infl_api_key = os.getenv('infl_api_key')
+from constants import infl_api_url
+from constants import infl_api_key
 
 from datetime import datetime
 import requests
